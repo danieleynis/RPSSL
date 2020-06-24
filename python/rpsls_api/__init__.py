6ac32623b00e70e7f6ef4745e40c62eca72a3a20
danieleynis <daniel.eynis@gmail.com>
2020-06-23 20:15:22 -0700

    /choices and /choice enpoints work

--- python/rpsls_api/__init__.py
+++ python/rpsls_api/__init__.py
@@ -1,20 +1,32 @@
-import os
+import random
 from flask import Flask
-
+from flask import render_template
+from flask import json
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
+    choice_list = ('rock', 'paper', 'scissors', 'lizard', 'spock')
+    num_choices = len(choice_list)
+    zipped_list = zip(range(1, num_choices+1), choice_list)
+    choices_id_list = [ { 'id': x, 'name': y } for x, y in zipped_list ]
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
     @app.route('/choices')
-    def get_choices():
-        return 'Hello World!'
+    def choices():
+        return json.jsonify(choices_id_list)
 
     @app.route('/choice')
-    def get_choice():
-        return 'Hello World!'
+    def choice():
+        return choices_id_list[random.randint(0, num_choices-1)]
+
 
     @app.route('/play', methods=['POST'])
     def play():
         return 'Hello World!'
+
 
     return app
